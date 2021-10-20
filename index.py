143e51fdb674f336d898effd3f66394143205b47
ami <amihayvasker12@gmail.com>
2021-10-20 17:21:54 +0300

    after change1 app1

--- index.py
+++ index.py
@@ -3,6 +3,6 @@ from flask import Flask
 
 app =Flask(__name__)
 @app.route('/')
-def hello_moshe():
-    return "hello moshe"
+def Hellow_Amihay():
+    return "hello Amihay"
 app.run(host="0.0.0.0",port=5000)
