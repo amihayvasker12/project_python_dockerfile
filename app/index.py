e7e57ec2b057287bb178d0097cd55f0e6f925534
ami <amihayvasker12@gmail.com>
2021-10-22 14:47:19 +0300

    after change1 app111

--- app/index.py
+++ app/index.py
@@ -5,4 +5,7 @@ app =Flask(__name__)
 @app.route('/')
 def Hellow_Amihay():
     return "hello Amihay"
-app.run(host="0.0.0.0",port=5000)
+@app.route('/moshe')
+def  amihay_vasker():
+    return "amihay vasker"
+app.run(host="127.0.0.1",port=5000)
